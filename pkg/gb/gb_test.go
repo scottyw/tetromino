7fde319d6dade2e9a7dad8936e921852bc3af02e
Scott Walker <scott.walker@puppet.com>
2018-05-22 18:08:45 +0100

    Automated Blargg test

---
+++ pkg/gb/gb_test.go
@@ -0,0 +1,93 @@
+package gb
+
+import (
+	"context"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/scottyw/tetromino/pkg/ui"
+)
+
+var (
+	blarggDir = os.Getenv("GOPATH") + "/src/github.com/scottyw/tetromino/test/blargg/"
+	timeout   = 5 * time.Second
+)
+
+func runBlarggTest(t *testing.T, filename string) {
+	sbWriter := &strings.Builder{}
+	opts := Options{
+		RomFilename: blarggDir + filename,
+		SBWriter:    sbWriter,
+		// DebugCPU:    true,
+	}
+	ctx, cancelFunc := context.WithTimeout(context.Background(), timeout)
+	defer cancelFunc()
+	gameboy := NewGameboy(ui.NewNop(), opts)
+	go func() {
+		for {
+			result := sbWriter.String()
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				if strings.Contains(result, "Failed") || strings.Contains(result, "Passed") {
+					cancelFunc()
+				}
+			}
+			// Check every 100ms for a result
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+	gameboy.Run(ctx)
+	<-ctx.Done()
+	result := sbWriter.String()
+	if !strings.Contains(result, "Passed") {
+		t.Errorf(result)
+	}
+}
+
+// func TestBlarggCpuInstrs01(t *testing.T) {
+// 	runBlarggTest(t, "cpu_instrs/01-special.gb")
+// }
+
+// func TestBlarggCpuInstrs02(t *testing.T) {
+// 	runBlarggTest(t, "cpu_instrs/02-interrupts.gb")
+// }
+
+// func TestBlarggCpuInstrs03(t *testing.T) {
+// 	runBlarggTest(t, "cpu_instrs/03-op sp,hl.gb")
+// }
+
+// func TestBlarggCpuInstrs04(t *testing.T) {
+// 	runBlarggTest(t, "cpu_instrs/04-op r,imm.gb")
+// }
+
+// func TestBlarggCpuInstrs05(t *testing.T) {
+// 	runBlarggTest(t, "cpu_instrs/05-op rp.gb")
+// }
+
+func TestBlarggCpuInstrs06(t *testing.T) {
+	runBlarggTest(t, "cpu_instrs/06-ld r,r.gb")
+}
+
+// func TestBlarggCpuInstrs07(t *testing.T) {
+// 	runBlarggTest(t, "cpu_instrs/07-jr,jp,call,ret,rst.gb")
+// }
+
+// func TestBlarggCpuInstrs08(t *testing.T) {
+// 	runBlarggTest(t, "cpu_instrs/08-misc instrs.gb")
+// }
+
+// func TestBlarggCpuInstrs09(t *testing.T) {
+// 	runBlarggTest(t, "cpu_instrs/09-op r,r.gb")
+// }
+
+// func TestBlarggCpuInstrs10(t *testing.T) {
+// 	runBlarggTest(t, "cpu_instrs/10-bit ops.gb")
+// }
+
+// func TestBlarggCpuInstrs11(t *testing.T) {
+// 	runBlarggTest(t, "cpu_instrs/11-op a,(hl).gb")
+// }
