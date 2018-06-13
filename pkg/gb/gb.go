722c1f23e1cd2f39eca52ea9b5298f664a2ddeb3
Scott Walker <scott.walker@puppet.com>
2018-06-13 18:56:35 +0100

    Count frames

--- pkg/gb/gb.go
+++ pkg/gb/gb.go
@@ -23,11 +23,12 @@ type Options struct {
 
 // Gameboy represents the Gameboy itself
 type Gameboy struct {
-	cpu *cpu.CPU
-	mem *mem.Memory
-	hwr *mem.HardwareRegisters
-	lcd *lcd.LCD
-	ui  ui.UI
+	cpu   *cpu.CPU
+	mem   *mem.Memory
+	hwr   *mem.HardwareRegisters
+	lcd   *lcd.LCD
+	ui    ui.UI
+	frame int
 }
 
 // NewGameboy returns a new Gameboy
@@ -39,7 +40,7 @@ func NewGameboy(ui ui.UI, opts Options) Gameboy {
 	return Gameboy{cpu: cpu, mem: mem, hwr: hwr, lcd: lcd, ui: ui}
 }
 
-func (gb Gameboy) runFrame() {
+func (gb *Gameboy) runFrame() {
 	// The Game Boy clock runs at 4.194304MHz
 	// There are 4 clock cycles to a "machine cycle" giving 1048576 machine cycles per second
 	// Each loop iteration below represents one machine cycle (i.e. 4 clock cycles)
@@ -53,10 +54,16 @@ func (gb Gameboy) runFrame() {
 	if gb.ui.UserInput().InputRecv {
 		gb.cpu.Start()
 	}
+	// FIXME maybe make this a flaggable feature?
+	// if gb.frame > 500 {
+	// 	time.Sleep(1000 * time.Millisecond)
+	// }
+	// fmt.Println(gb.frame)
+	gb.frame++
 }
 
 // Run the Gameboy
-func (gb Gameboy) Run(ctx context.Context) {
+func (gb *Gameboy) Run(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
@@ -69,7 +76,7 @@ func (gb Gameboy) Run(ctx context.Context) {
 }
 
 // Time the Gameboy as it runs
-func (gb Gameboy) Time(ctx context.Context) {
+func (gb *Gameboy) Time(ctx context.Context) {
 	for {
 		// There are just under 60 frames per second (59.7275) so let's time in blocks of 60 frames
 		// On a real Gameboy this would take 1 second
