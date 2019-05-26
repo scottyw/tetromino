465290431a9f78858bfeff98c6532d57764734da
Scott Walker <scott.walker@puppet.com>
2019-05-26 16:50:24 +0100

    Complete copy of blargg test roms

--- pkg/gb/blargg_test.go
+++ pkg/gb/blargg_test.go
@@ -41,62 +41,6 @@ func runBlarggTest(t *testing.T, filename string) {
 	}
 }
 
-func TestBlargg00(t *testing.T) { runBlarggTest(t, "cpu_instrs.gb") }
+func TestBlarggCPU(t *testing.T) { runBlarggTest(t, "cpu_instrs/cpu_instrs.gb") }
 
-// func TestBlargg12(t *testing.T) { runBlarggTest(t, "dmg_sound-2.gb") }
-
-// func TestBlargg13(t *testing.T) { runBlarggTest(t, "dmg_sound-2/01-registers.gb") }
-
-// func TestBlargg14(t *testing.T) { runBlarggTest(t, "dmg_sound-2/02-len ctr.gb") }
-
-// func TestBlargg15(t *testing.T) { runBlarggTest(t, "dmg_sound-2/03-trigger.gb") }
-
-// func TestBlargg16(t *testing.T) { runBlarggTest(t, "dmg_sound-2/04-sweep.gb") }
-
-// func TestBlargg17(t *testing.T) { runBlarggTest(t, "dmg_sound-2/05-sweep details.gb") }
-
-// func TestBlargg18(t *testing.T) { runBlarggTest(t, "dmg_sound-2/06-overflow on trigger.gb") }
-
-// func TestBlargg19(t *testing.T) { runBlarggTest(t, "dmg_sound-2/07-len sweep period sync.gb") }
-
-// func TestBlargg20(t *testing.T) { runBlarggTest(t, "dmg_sound-2/08-len ctr during power.gb") }
-
-// func TestBlargg21(t *testing.T) { runBlarggTest(t, "dmg_sound-2/09-wave read while on.gb") }
-
-// func TestBlargg22(t *testing.T) { runBlarggTest(t, "dmg_sound-2/10-wave trigger while on.gb") }
-
-// func TestBlargg23(t *testing.T) { runBlarggTest(t, "dmg_sound-2/11-regs after power.gb") }
-
-// func TestBlargg24(t *testing.T) { runBlarggTest(t, "dmg_sound-2/12-wave write while on.gb") }
-
-// func TestBlargg25(t *testing.T) { runBlarggTest(t, "halt_bug.gb") }
-
-func TestBlargg26(t *testing.T) { runBlarggTest(t, "instr_timing.gb") }
-
-// func TestBlargg27(t *testing.T) { runBlarggTest(t, "interrupt_time.gb") }
-
-// func TestBlargg28(t *testing.T) { runBlarggTest(t, "mem_timing-2.gb") }
-
-// func TestBlargg29(t *testing.T) { runBlarggTest(t, "mem_timing-2/01-read_timing.gb") }
-
-// func TestBlargg30(t *testing.T) { runBlarggTest(t, "mem_timing-2/02-write_timing.gb") }
-
-// func TestBlargg31(t *testing.T) { runBlarggTest(t, "mem_timing-2/03-modify_timing.gb") }
-
-// func TestBlargg32(t *testing.T) { runBlarggTest(t, "oam_bug-2.gb") }
-
-// func TestBlargg33(t *testing.T) { runBlarggTest(t, "oam_bug-2/1-lcd_sync.gb") }
-
-// func TestBlargg34(t *testing.T) { runBlarggTest(t, "oam_bug-2/2-causes.gb") }
-
-// func TestBlargg35(t *testing.T) { runBlarggTest(t, "oam_bug-2/3-non_causes.gb") }
-
-// func TestBlargg36(t *testing.T) { runBlarggTest(t, "oam_bug-2/4-scanline_timing.gb") }
-
-// func TestBlargg37(t *testing.T) { runBlarggTest(t, "oam_bug-2/5-timing_bug.gb") }
-
-// func TestBlargg38(t *testing.T) { runBlarggTest(t, "oam_bug-2/6-timing_no_bug.gb") }
-
-// func TestBlargg39(t *testing.T) { runBlarggTest(t, "oam_bug-2/7-timing_effect.gb") }
-
-// func TestBlargg40(t *testing.T) { runBlarggTest(t, "oam_bug-2/8-instr_effect.gb") }
+func TestBlarggInstrTiming(t *testing.T) { runBlarggTest(t, "instr_timing/instr_timing.gb") }
