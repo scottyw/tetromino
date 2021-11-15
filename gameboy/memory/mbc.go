6b424ea1dc255b32d3ad5ef8268964164b802ab1
Scott Walker <smwalker@gmail.com>
2021-11-15 20:42:10 +0000

    MBC refactor

--- gameboy/memory/mbc.go
+++ gameboy/memory/mbc.go
@@ -4,40 +4,105 @@ import (
 	"fmt"
 )
 
-type mbc struct {
-	// ROM and RAM data and mask read from the cart
-	rom [][0x4000]byte
-	ram [][0x2000]byte
-
-	// Record of what as written between 0x0000 and 0x8000
-	enabledRegion uint8
-	romRegion     uint8
-	ramRegion     uint8
-	modeRegion    uint8
-
-	// Selected ROM and RAM banks computed from written values on an MBC-type basis
-	ramEnabled bool
-	romBank0   int
-	romBankX   int
-	ramBank    int
-	update     func(*mbc)
+type mbc interface {
+	Read(addr uint16) uint8
+	Write(addr uint16, value uint8)
+	DumpRAM() []byte
 }
 
-func newMBC(rom []byte) *mbc {
-	// FIXME this is really for unit tests - need to be more graceful about this and missing ROM files in general
-	if rom == nil || len(rom) < 0x0148 {
+type none struct {
+	rom []byte
+}
+
+func (n *none) Read(addr uint16) uint8 {
+	return n.rom[addr]
+}
+
+func (n *none) Write(addr uint16, value uint8) {
+	// Do nothing
+}
+
+func (n *none) DumpRAM() []byte {
+	return []byte{}
+}
+
+func newMBC(romImage []byte) mbc {
+
+	if romImage == nil || len(romImage) < 0x0148 {
 		return nil
 	}
-	cartType := rom[0x0147]
-	romSize := rom[0x0148]
-	ramSize := rom[0x0149]
-	return &mbc{
-		rom:      splitROMIntoPages(romSize, rom),
-		ram:      createRAM(cartType, ramSize),
-		romBank0: 0,
-		romBankX: 1,
-		update:   chooseUpdateFunc(cartType),
+	cartType := romImage[0x0147]
+	romSize := romImage[0x0148]
+	rom := splitROMIntoPages(romSize, romImage)
+	ramSize := romImage[0x0149]
+	ram := createRAM(cartType, ramSize)
+
+	switch cartType {
+	case 0x00:
+		// 00 - ROM ONLY
+		return &none{rom: romImage}
+	case 0x01:
+		// 01 - ROM + MBC1
+		return newMBC1(rom, ram)
+	case 0x02:
+		// 02 - ROM + MBC1 + RAM
+		return newMBC1(rom, ram)
+	case 0x03:
+		// 03 - ROM + MBC1 + RAM + BATT
+		return newMBC1(rom, ram)
+	case 0x05:
+		// 05 - ROM + MBC2
+	case 0x06:
+		// 06 - ROM + MBC2 + BATT
+	case 0x08:
+		// 08 - ROM + RAM
+	case 0x09:
+		// 09 - ROM + RAM + BATT + MMM01
+	case 0x0c:
+		// 0C - ROM + MMM01 + SRAM
+	case 0x0d:
+		// 0D - ROM + MMM01 + SRAM + BATT
+	case 0x0f:
+		// 0f - ROM + MBC3 + TIMER + BATT
+		return newMBC3(rom, ram)
+	case 0x10:
+		// 10 - ROM + MBC3 + RAM + TIMER + BATT
+		return newMBC3(rom, ram)
+	case 0x11:
+		// 11 - ROM + MBC3
+		return newMBC3(rom, ram)
+	case 0x12:
+		// 12 - ROM + MBC3 + RAM
+		return newMBC3(rom, ram)
+	case 0x13:
+		// 13 - ROM + MBC3 + RAM + BATT
+		return newMBC3(rom, ram)
+	case 0x19:
+		// 19 - ROM + MBC5
+	case 0x1a:
+		// 1A - ROM + MBC5 + RAM
+	case 0x1b:
+		// 1B - ROM + MBC5 + RAM + BATT
+	case 0x1c:
+		// 1C - ROM + MBC5 + RUMBLE
+	case 0x1d:
+		// 1D - ROM + MBC5 + RUMBLE + SRAM
+	case 0x1e:
+		// 1E - ROM + MBC5 + RUMBLE + SRAM + BATT
+	case 0x20:
+		// 20 - ROM + MBC6 + RAM + BATT
+	case 0x22:
+		// 22 - ROM + MBC7 + RAM + BATT + ACCELEROMETER
+	case 0xfc:
+		// FC - POCKET CAMERA
+	case 0xfd:
+		// FD - Bandai TAMA5
+	case 0xfe:
+		// FE - Hudson on HuC-1
+	case 0xff:
+		// FF - Hudson on HuC-1 + RAM + BATTERY
 	}
+	panic(fmt.Sprintf("mbc does not support cart type 0x%02x", cartType))
 }
 
 func splitROMIntoPages(romSize uint8, rom []byte) [][0x4000]byte {
@@ -83,169 +148,4 @@ func createRAM(cartType, ramSize uint8) [][0x2000]byte {
 		}
 	}
 	return ram
-}
-
-func chooseUpdateFunc(cartType uint8) func(*mbc) {
-	switch cartType {
-	case 0x00:
-		// 00 - ROM ONLY
-		return func(_ *mbc) {}
-	case 0x01:
-		// 01 - ROM + MBC1
-		return updateMBC1
-	case 0x02:
-		// 02 - ROM + MBC1 + RAM
-		return updateMBC1
-	case 0x03:
-		// 03 - ROM + MBC1 + RAM + BATT
-		return updateMBC1
-	case 0x05:
-		// 05 - ROM + MBC2
-	case 0x06:
-		// 06 - ROM + MBC2 + BATT
-	case 0x08:
-		// 08 - ROM + RAM
-	case 0x09:
-		// 09 - ROM + RAM + BATT + MMM01
-	case 0x0c:
-		// 0C - ROM + MMM01 + SRAM
-	case 0x0d:
-		// 0D - ROM + MMM01 + SRAM + BATT
-	case 0x0f:
-		// 0f - ROM + MBC3 + TIMER + BATT
-		return updateMBC3
-	case 0x10:
-		// 10 - ROM + MBC3 + RAM + TIMER + BATT
-		return updateMBC3
-	case 0x11:
-		// 11 - ROM + MBC3
-		return updateMBC3
-	case 0x12:
-		// 12 - ROM + MBC3 + RAM
-		return updateMBC3
-	case 0x13:
-		// 13 - ROM + MBC3 + RAM + BATT
-		return updateMBC3
-	case 0x19:
-		// 19 - ROM + MBC5
-	case 0x1a:
-		// 1A - ROM + MBC5 + RAM
-	case 0x1b:
-		// 1B - ROM + MBC5 + RAM + BATT
-	case 0x1c:
-		// 1C - ROM + MBC5 + RUMBLE
-	case 0x1d:
-		// 1D - ROM + MBC5 + RUMBLE + SRAM
-	case 0x1e:
-		// 1E - ROM + MBC5 + RUMBLE + SRAM + BATT
-	case 0x20:
-		// 20 - ROM + MBC6 + RAM + BATT
-	case 0x22:
-		// 22 - ROM + MBC7 + RAM + BATT + ACCELEROMETER
-	case 0xfc:
-		// FC - POCKET CAMERA
-	case 0xfd:
-		// FD - Bandai TAMA5
-	case 0xfe:
-		// FE - Hudson on HuC-1
-	case 0xff:
-		// FF - Hudson on HuC-1 + RAM + BATTERY
-	}
-	panic(fmt.Sprintf("mbc does not support cart type 0x%02x", cartType))
-}
-
-func (m *mbc) read(addr uint16) uint8 {
-	switch {
-	case addr < 0x4000:
-		return m.rom[m.romBank0][addr]
-	case addr < 0x8000:
-		offset := addr - 0x4000
-		return m.rom[m.romBankX][offset]
-	case addr < 0xa000:
-		panic(fmt.Sprintf("mbc has no read mapping for address 0x%04x", addr))
-	case addr < 0xc000:
-		if m.ramEnabled {
-			offset := addr - 0xa000
-			return m.ram[m.ramBank][offset]
-		}
-		return 0xff
-	default:
-		panic(fmt.Sprintf("mbc has no read mapping for address 0x%04x", addr))
-	}
-}
-
-func (m *mbc) write(addr uint16, value uint8) {
-	switch {
-	case addr < 0x2000:
-		m.enabledRegion = value
-	case addr < 0x4000:
-		m.romRegion = value
-	case addr < 0x6000:
-		m.ramRegion = value
-	case addr < 0x8000:
-		m.modeRegion = value
-	case addr < 0xa000:
-		panic(fmt.Sprintf("mbc has no write mapping for address 0x%04x", addr))
-	case addr < 0xc000:
-		offset := addr - 0xa000
-		if m.ramEnabled {
-			m.ram[m.ramBank][offset] = value
-		}
-	default:
-		panic(fmt.Sprintf("mbc has no write mapping for address 0x%04x", addr))
-	}
-	m.update(m)
-}
-
-func updateMBC1(m *mbc) {
-
-	// Check if RAM is enabled
-	m.ramEnabled = m.enabledRegion&0x0f == 0x0a
-
-	// Check ROM bank 0
-	if m.modeRegion&0x01 == 0 {
-		m.romBank0 = 0
-	} else {
-		m.romBank0 = int((m.ramRegion & 0x03) << 5)
-		m.romBank0 = m.romBank0 % len(m.rom)
-	}
-
-	// Check ROM bank 1
-	m.romBankX = int((m.romRegion & 0x1f))
-	if m.romBankX == 0 {
-		m.romBankX = 1
-	}
-	m.romBankX |= int((m.ramRegion & 0x03 << 5))
-	m.romBankX = m.romBankX % len(m.rom)
-
-	// Check RAM bank
-	if m.ramEnabled {
-		if m.modeRegion&0x01 == 0 {
-			m.ramBank = 0
-		} else {
-			m.ramBank = int(m.ramRegion & 0x03)
-			m.ramBank = m.ramBank % len(m.ram)
-		}
-	}
-
-}
-
-func updateMBC3(m *mbc) {
-
-	// Check if RAM is enabled
-	m.ramEnabled = m.enabledRegion&0x0f == 0x0a
-
-	// Check ROM bank 1
-	m.romBankX = int((m.romRegion & 0x7f))
-	if m.romBankX == 0 {
-		m.romBankX = 1
-	}
-	m.romBankX = m.romBankX % len(m.rom)
-
-	// Check RAM bank
-	if m.ramEnabled {
-		m.ramBank = int(m.ramRegion & 0x07)
-		m.ramBank = m.ramBank % len(m.ram)
-	}
-
 }
